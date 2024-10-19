f1890de022a0c331bde7f8132bc59bbfa5f217c1
unknown <goyalsumit651@gmail.com>
2024-10-19 19:26:18 +0530

    Second Commit

--- backend/server.js
+++ backend/server.js
@@ -11,4 +11,4 @@ app.use(bodyParser.json());
 app.use(session({ secret: process.env.SECRET_COOKIE, resave: false, saveUninitialized: true,cookie : {secure : false} }));
 mongoose.connect(process.env.MONGODB_URI).then(()=>console.log("Database connected")).catch((error)=>console.log("Error connecting database",error));
 app.use('/auth', authRoutes);
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.listen(5000, () => console.log(`Server running on port ${process.env.PORT}`));
